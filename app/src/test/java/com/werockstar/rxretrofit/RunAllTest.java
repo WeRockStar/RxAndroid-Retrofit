ec239f88aa3fb6df56d395e9676306719d4972e5
WeRockStar <vr_freestyle@outlook.com>
2016-09-25 20:59:37 +0700

    Added class for run all test case

---
+++ app/src/test/java/com/werockstar/rxretrofit/RunAllTest.java
@@ -0,0 +1,11 @@
+package com.werockstar.rxretrofit;
+
+import com.werockstar.rxretrofit.presenter.GithubPresenterTest;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({GithubPresenterTest.class, ExampleUnitTest.class})
+public class RunAllTest {
+}
