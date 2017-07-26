563e8088600950c926d238bce7a24f9a47f73904
WeRockStar <kotchaphan.m@gmail.com>
2017-07-26 21:58:09 +0700

    Refactor to use composite subscription

--- app/src/main/java/com/werockstar/rxretrofit/presenter/GithubPresenter.java
+++ app/src/main/java/com/werockstar/rxretrofit/presenter/GithubPresenter.java
@@ -3,13 +3,13 @@ package com.werockstar.rxretrofit.presenter;
 import com.werockstar.rxretrofit.api.GithubAPI;
 import com.werockstar.rxretrofit.model.GithubCollection;
 
-import rx.Subscription;
 import rx.android.schedulers.AndroidSchedulers;
 import rx.schedulers.Schedulers;
+import rx.subscriptions.CompositeSubscription;
 
 public class GithubPresenter {
 
-    private Subscription subscribe;
+    private CompositeSubscription subscription = new CompositeSubscription();
     private GithubAPI api;
     private GithubPresenter.View view;
 
@@ -27,19 +27,18 @@ public class GithubPresenter {
     }
 
     public void getGithubInfo(String username) {
-        subscribe = api.getGithubInfo(username)
+        subscription.add(api.getGithubInfo(username)
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
                 .subscribe(
                         github -> view.showGithubInfo(github),
                         throwable -> view.onError(throwable),
                         () -> view.onCompleted()
-                );
+                ));
     }
 
     public void onStop() {
-        if (subscribe != null && !subscribe.isUnsubscribed())
-            subscribe.unsubscribe();
+        subscription.clear();
     }
 
 }
